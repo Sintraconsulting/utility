77cef8f611003c6a00c65e453f19ea5b09b916b0
sintrahq <87361279+sintrahq@users.noreply.github.com>
2024-09-17 11:09:02 +0200

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-SERVICE="https://www.google.com"
-
+# Ciclo infinito per eseguire lo script ogni 5 secondi
 while true; do
-  if ! curl -s --head $SERVICE | grep "HTTP/2 200" > /dev/null; then
-    echo "$(date): Connessione a $SERVICE fallita" >> /dev/stdout
+  # Effettua la chiamata con curl e cattura l'output e gli errori
+  OUTPUT=$(curl -sS --head https://ttprod.posteitaliane.it 2>&1)
+
+  # Verifica se l'errore è dovuto al codice di errore 6 (Could not resolve host)
+  if [[ $? -eq 6 ]]; then
+    echo "Errore: Could not resolve host" >&2
   fi
+
+  # Attende 5 secondi prima di ripetere il ciclo
   sleep 5
 done
